bce06c3caa2f6426ddb1d8e9083f2cdfc7fa19a2
james reantillo <james.reantillo@gmail.com>
2020-12-16 00:21:42 -0800

    redux setup update

--- src/reducers/gamesReducer.js
+++ src/reducers/gamesReducer.js
@@ -1,1 +1,16 @@
-const gameReducer = (state, action) => {};
+const initState = {
+  popular: [],
+  newGames: [],
+  upComing: [],
+  searched: [],
+};
+const gamesReducer = (state = initState, action) => {
+  switch (action.type) {
+    case 'FETCH_GAMES':
+      return { ...state };
+    default:
+      return { ...state };
+  }
+};
+
+export default gamesReducer;
