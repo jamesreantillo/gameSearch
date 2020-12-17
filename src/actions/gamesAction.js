fca017fc32d22e9c6cdb1cdcba38ae05bc7256b4
james reantillo <james.reantillo@gmail.com>
2020-12-16 19:33:16 -0800

    fetch upcoming and new games

--- src/actions/gamesAction.js
+++ src/actions/gamesAction.js
@@ -1,15 +1,19 @@
 import axios from 'axios';
-import { popularGamesURL } from '../api';
+import { popularGamesURL, upcomimgGamesURL, newGamesURL } from '../api';
 
 //
 
 export const loadGames = () => async (dispatch) => {
   //FETCH AXIOS
   const popularData = await axios.get(popularGamesURL());
+  const newGamesData = await axios.get(newGamesURL());
+  const upcomingData = await axios.get(upcomimgGamesURL());
   dispatch({
     type: 'FETCH_GAMES',
     payload: {
       popular: popularData.data.results,
+      upcoming: upcomingData.data.results,
+      newGames: newGamesData.data.results,
     },
   });
 };
