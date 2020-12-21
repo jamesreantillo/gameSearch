b041a850190db3f2d5c4a90493bf391bdc58980f
james reantillo <james.reantillo@gmail.com>
2020-12-20 18:46:11 -1000

    wait before fetching detail/ isLoading

--- src/actions/detailAction.js
+++ src/actions/detailAction.js
@@ -1,10 +1,13 @@
 import axios from 'axios';
 import { gamesDetailsURL, gamesScreenShotURL } from '../api';
 
-export const loadDetail = (id) => async (disptach) => {
+export const loadDetail = (id) => async (dispatch) => {
+  dispatch({
+    type: 'LOADING_DETAIL',
+  });
   const detailData = await axios.get(gamesDetailsURL(id));
   const screenShotData = await axios.get(gamesScreenShotURL(id));
-  disptach({
+  dispatch({
     type: 'GET_DETAIL',
     payload: {
       game: detailData.data,
